5cd2e599f91df6485aa5b0c48e5b59c6c9717e2a
Ahmed Orabi <eng.ahmedorabi94@gmail.com>
2022-11-20 20:52:15 +0200

    add ui test for HomeFragment

---
+++ app/src/androidTest/java/com/ahmedorabi/trackingapp/features/home/HomeFragmentTest.kt
@@ -0,0 +1,51 @@
+package com.ahmedorabi.trackingapp.features.home
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ahmedorabi.trackingapp.R
+import com.ahmedorabi.trackingapp.launchFragmentInHiltContainer
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeFragmentTest {
+
+
+    @Test
+    fun testHomeFragment() {
+
+
+        launchFragmentInHiltContainer<HomeFragment>()
+
+        onView(withId(R.id.startBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.resumeBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.endBtn)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun test_start_button_HomeFragment() {
+
+
+        launchFragmentInHiltContainer<HomeFragment>()
+
+
+        onView(withId(R.id.startBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.resumeBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.endBtn)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.startBtn)).perform(click())
+
+        Thread.sleep(2000)
+
+        //  onView(withId(R.id.distanceTv)).check(matches(isDisplayed()))
+        // onView(withId(R.id.stepsTv)).check(matches(isDisplayed()))
+        onView(withId(R.id.trackTimeTv)).check(matches(isDisplayed()))
+        onView(withId(R.id.circularProgressBar)).check(matches(isDisplayed()))
+
+
+    }
+}
